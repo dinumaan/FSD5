e272cc0f782d586ff5cf98bfa2c2bd9ef0c9519a
dinumaan <dineshmatrix675@gmail.com>
2022-08-17 19:53:15 +0530

    added code file of 17th august

---
+++ 17_aug.js
@@ -0,0 +1,68 @@
+// 1234
+// 123
+// 12
+// 1
+
+// 4
+// 34
+// 234
+// 1234
+// let n = 4, start;
+// for(let row = 0; row < n; row++){
+//     start = n - row;
+//     for(let col = 0; col <= row; col++){
+//         process.stdout.write(start.toString())
+//         start++;
+//     }
+//     console.log("");
+// }
+
+// 3
+// 23
+// 123
+
+// let arr = [3, 4, 12, 34, 36];
+// // insert 45 to the end of the array 
+// arr.push(45);
+
+// console.log(arr);
+
+// // remove the last element of an array ?
+
+// arr.pop();
+
+// console.log(arr);
+
+// let arr1 = arr.slice(2,5);
+
+
+// console.log(arr1);
+
+// let arr = [1, 34, 56, 23, 45];
+// // 67 at postion 2nd => [1, 67, 34, 56, 23, 45]
+
+// arr.push(67);
+
+// // [1, 34, 56, 23, 45, 67]
+// // [1, 67,34, 56, 23, 45]
+
+// for(let i = arr.length-2; i >= 1; i--){
+//     arr[i+1] = arr[i];
+// }
+
+// arr[1] = 67;
+
+// console.log(arr);
+
+//  function expression and function decalartion ?
+// const addTwo = (x, y) => x+y;
+
+// console.log(addTwo(2,3));
+
+
+// curing and argument binding 
+// function addTwo(x, y){
+//     return x+y;
+// }
+
+
