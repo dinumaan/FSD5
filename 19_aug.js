5ab0104946dcb026bae943fa834429c880308908
dinumaan <dineshmatrix675@gmail.com>
2022-08-19 23:04:50 +0530

    added code file of 19th august

---
+++ 19_aug.js
@@ -0,0 +1,96 @@
+// 2. Program to clone the object and change the property and
+// then iterate the array of objects using inbuilt functions
+// and to console the object property and frame a meaning full sentence.
+
+let arr = [
+  { name: "Dinesh", class: "FSD5" },
+  { name: "Santosh", class: "FSD5" },
+  { name: "kultaj", class: "FSD5" },
+];
+
+// let arr = [1, 3, 5, 12];
+
+// arr.forEach( (x) => {
+//     // play with object x
+
+// });
+
+// for(let i = 0; i < arr.length; i++){
+//     // console.log(arr[i].name, arr[i].class);
+//     for(let j in arr[i]){
+//         console.log(arr[i][j]);
+//     }
+// }
+
+// let myObj = {
+//     name: "Dinesh",
+//     class: "FSD5",
+//     location: "Kerla"
+// }
+
+// let clonedObj = {};
+
+// Object.assign(clonedObj, myObj);
+
+// clonedObj.name = "Santhosh";
+// clonedObj.location = "TmilNadu";
+
+// console.log(myObj);
+// console.log(clonedObj);
+
+// (a+b) square (a+b)*(a+b) => a*a + b*b + 2*a*b
+
+// function sovle(a, b){
+//     return (a+b)*(a+b);
+// }
+
+// console.log(sovle(3,4));
+
+// x square => x*x
+// 5 square is 5*5
+// 6 square is
+
+// let n = 4;
+// let ans = 1;
+
+// while(n > 0){
+//     ans = (ans*n);
+//     n--;
+// }
+
+// console.log(ans);
+
+// function isPrime(n) {
+//   if (n <= 1) return false;
+
+//   for (let i = 2; i < n; i++) if (n % i == 0) return false;
+
+//   return true;
+// }
+
+// function printPrime(n) {
+//   for (let i = 2; i <= n; i++) {
+//     if (isPrime(i)) console.log(i + " ");
+//   }
+// }
+
+// let n = 5;
+// printPrime(n);
+// let arrr = [{ a: "asd", b: 23 }, {c:"fgh", d: 24}];
+
+// for (let r of arrr) {
+// //   for (let f in r) {
+// //     console.log(r[f]);
+// //   }
+
+// }
+
+// Write a method that accepts an array of integers 
+// and updates every element with multiplication of previous and next elements.
+
+// function solve(arr){
+
+// }
+
+
+// [1, 2, 3, 4, 5]
